7de81ce9634fc572b8d8066f5f24bf85e8e218b8
AlphaLEXray <alphalexray@protonmail.com>
2022-01-14 16:36:15 +0100

    Improve script logic

--- git-change-name-email-script.bash
+++ git-change-name-email-script.bash
@@ -1,19 +1,26 @@
 #!/bin/bash
 echo "This is script is an easy way to change your Git username and/or email. Just enter your details when asked to and the script will do the rest."
 echo
-read -p 'Git-Username: ' uservar
-read -p 'Git-Email: ' emailvar
+read -p 'Git-Username: ' uservar #enter username
+read -p 'Git-Email: ' emailvar #enter email
 echo
 echo Your name is $uservar and your email is $emailvar
-read -p 'Is this corret? Please type yes or no: ' ynvar
-if [$read == $no] #if input = no
+read -p 'Is this correct? Please type yes or no: ' ynvar
+if [ $ynvar == "yes" ]
 then
-	$quit #quits the script
-else
 	echo Thank you for confirming. You have set your name as $uservar and your email as $emailvar.
+	echo
+	git config --global user.name "$uservar" #set git user
+	git config --global user.email "$emailvar" #set git email
+	echo 'Username: ' && git config --get user.name
+	echo 'Email: ' && git config --get user.email
+elif [ $ynvar == "no" ]
+then
+	echo
+	echo Please execute the script again.
+	exit
+else
+	echo
+	echo Input not recognised. Please execute the script again.
+	exit
 fi
-echo
-git config --global user.name "$uservar" #sets username
-git config --global user.email "$emailvar" #sets email
-echo 'Username: ' && git config --get user.name
-echo 'Email: ' && git config --get user.email
